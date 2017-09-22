2663448b6b3fea01cfa61aff49132fec3ab90b4a
VladFedotov <32197532+VladFedotov@users.noreply.github.com>
2017-09-22 19:49:49 +0300

    Add files via upload

--- js/jquery.flying-labels.js
+++ js/jquery.flying-labels.js
@@ -1,6 +1,7 @@
 ;(function($){
     var methods = {
-        show: function(dataFor, config){
+        show: function(e){
+            var dataFor = $(e.target).attr('name');
             var $placeholder = $("."+config.placeholderClass+"[data-for='"+dataFor+"']");
             var $field = $("[name='"+dataFor+"']");
             if($field.val()!=""){
@@ -13,37 +14,42 @@
                 }else $placeholder.first().css('animation', config.showAnimation+' forwards '+config.animationTime+'ms').show();
             }  
         },  
-        hide: function(dataFor, config){
+        hide: function(e){
+            var dataFor = $(e.target).attr('name');
             var $placeholder = $("."+config.placeholderClass+"[data-for='"+dataFor+"']");
             var $field = $("[name='"+dataFor+"']");
             if($field.val()==""){
                 $placeholder.first().css('animation', config.hideAnimation+' forwards '+config.animationTime/2+'ms');
             }
             
-        }
-    }
-    $.fn.fly = function(options){
-        var config = $.extend({
-            'wrapperClass': 'it-wrapper',
-            'placeholderClass': 'placeholder',
-            'animationTime': '200',
-            'showAnimation': 'slideInUp',
-            'hideAnimation': 'zoomOut'
-        }, options);
-        return this.each(function(){    
-            if($(this)[0].tagName==="INPUT" && $(this).attr('type')!="password" && $(this).attr('type')!="text") return;
-            if($(this)[0].tagName!=="INPUT" && $(this)[0].tagName!=="TEXTAREA") return;
-            if(typeof $(this).attr('placeholder')==="undefined") return;
-            if(!$(this).parent().hasClass(config.wrapperClass)) $(this).wrap("<div class='"+config.wrapperClass+"'/>");
-            if($(this).val().trim()!=""){
+        },
+        init: function(obj){
+            if(obj[0].tagName==="INPUT" && obj.attr('type')!="password" && obj.attr('type')!="text") return;
+            if(obj[0].tagName!=="INPUT" && obj[0].tagName!=="TEXTAREA") return;
+            if(typeof obj.attr('placeholder')==="undefined") return;
+            if(!obj.parent().hasClass(config.wrapperClass)) obj.wrap("<div class='"+config.wrapperClass+"'/>");
+            if(obj.val().trim()!=""){
                 $("<div/>", {
                     "class": config.placeholderClass,
-                    "data-for": $(this).attr('name'),
-                    text: $(this).attr('placeholder')
-                }).insertAfter($(this)).fadeIn(config.animationTime);
+                    "data-for": obj.attr('name'),
+                    text: obj.attr('placeholder')
+                }).insertAfter(obj).fadeIn(config.animationTime);
             }
-            $(this).bind("keyup.fly", function(){methods.show($(this).attr('name'), config)});
-            $(this).bind("change.fly keyup.fly keydown.fly", function(){methods.hide($(this).attr('name'), config)});
+            obj.bind("keyup.fly", methods.show);
+            obj.bind("change.fly keyup.fly keydown.fly", methods.hide);
+        }
+    }
+    var config, defaults = {
+        'wrapperClass': 'it-wrapper',
+        'placeholderClass': 'placeholder',
+        'animationTime': '200',
+        'showAnimation': 'slideInUp',
+        'hideAnimation': 'zoomOut'
+    };
+    $.fn.fly = function(options){
+        config = $.extend(defaults, options);
+        return this.each(function(){    
+            methods.init($(this));
         });
     }
 })(jQuery);
